65751abe554394d95190c88628a711dc20717a7f
RAKSHIT2006 <72135785+RAKSHIT2006@users.noreply.github.com>
2021-03-29 17:37:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var palyer1,player2,player3,player4;
+ 
+function preload(){
+	ground = loadImage("images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if (gameState === 2){
+    game.end()
+  }
+}
+
+
+
+
+
+
+
